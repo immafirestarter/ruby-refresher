ae562a6441b5ab6ba3549aa61db8e1ce31a9940c
Sami <sami@Samis-MBP.home>
2017-05-29 21:46:11 +0100

    a few more

--- lib/questions.rb
+++ lib/questions.rb
@@ -84,29 +84,36 @@ end
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
+  array.sort_by{|s| s.length }[0]
 end
 
 # return the shortest word in an array
 def longest_word_in_array(array)
+  array.sort_by{|s| s.length }[-1]
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
 # returns 15
 def total_of_array(array)
+  array.sum
 end
 
 # turn an array into itself repeated twice. So [1, 2, 3]
 # becomes [1, 2, 3, 1, 2, 3]
 def double_array(array)
+  array*2
 end
 
 # convert a symbol into a string
 def turn_symbol_into_string(symbol)
+  symbol.to_s
 end
 
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 17
 def average_of_array(array)
+  b = array.length
+  (array.sum/b.to_f).round
 end
 
 # get all the elements in an array, up until the first element
